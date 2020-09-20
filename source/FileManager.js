6eae05d96c9bebff255f7bb0bd0c452b00a666bb
mauricio.montellano <ivar.montellano@gmail.com>
2020-09-20 14:55:05 +0200

    create html table from matrix

--- source/FileManager.js
+++ source/FileManager.js
@@ -10,4 +10,25 @@ async function convertToMatrix(urlPath){
     return $.csv.toArrays(data);
 }
 
-convertToMatrix("./source/resources/dataset.csv").then(response => console.log(response));
+function createTable(tableData) {
+    var table = document.createElement('table');
+    var tableBody = document.createElement('tbody');
+  
+    tableData.forEach(function(rowData) {
+      var row = document.createElement('tr');
+  
+      rowData.forEach(function(cellData) {
+        var cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(cellData));
+        row.appendChild(cell);
+      });
+  
+      tableBody.appendChild(row);
+    });
+  
+    table.appendChild(tableBody);
+    document.body.appendChild(table);
+  }
+
+convertToMatrix("./source/resources/dataset.csv").then( response => createTable(response)
+    );
